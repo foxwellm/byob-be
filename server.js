57e8e0b48d65ba172f0947ef407d723f2fae8f5f
Matthew Foxwell <foxwellmw@gmail.com>
2019-03-13 10:54:42 -0600

    Add post requests

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+// import cors from 'cors'
 const app = express();
-
+app.use(express.json())
+// app.use(cors())
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
@@ -64,3 +66,52 @@ app.get('/api/v1/skins/:id', (request, response) => {
     });
 });
 
+app.post('/api/v1/champions', (request, response) => {
+  const champion = request.body;
+
+  for (let requiredParameter of ['name', 'title']) {
+    if (!champion[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, title: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('champions').insert(champion, 'id')
+    .then(champion => {
+      response.status(201).json({ id: champion[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/skins', (request, response) => {
+  const skin = request.body;
+
+  for (let requiredParameter of ['skin_name', 'chroma', 'champion']) {
+    if (!skin[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { skin_name: <String>, chroma: <Boolean>, champion: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+  database('champions').where('name', skin.champion).select()
+    .then(champions => {
+      if (champions.length) {
+          let newSkin = { skin_name: skin.skin_name, chroma: skin.chroma, champion_id: champions[0].id }
+        database('skins').insert(newSkin, 'id')
+          .then(skin => {
+            response.status(201).json({ id: skin[0] })
+          })
+      } else {
+        response.status(404).json({
+          error: `Could not find champion_id associated with champion ${skin.champion}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+
+});
