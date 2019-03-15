9528fb825ac3a3385d62dd19b0ecf3c8703aa8f1
Matthew Foxwell <foxwellmw@gmail.com>
2019-03-15 09:47:23 -0600

    Add heroku

--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,13 @@ module.exports = {
     seeds: {
       directory: './db/seeds/development'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
